bf3f2c2bd55c3e32a70f3e57eee036c3101dd5cb
Jonathan Silva <jonalves.silva@jsmail.com.br>
2023-05-17 10:07:02 -0300

    Criacao Componentes e Home.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,17 +4,19 @@ import "./App.css";
 import { Appbar } from "./components/Appbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./routes/Home";
+import { TabelaIntegrantes } from "./components/TabelaIntegrantes";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
+    <BrowserRouter>
+      <div className="mb-16">
         <Appbar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/integrantes" element={<TabelaIntegrantes />}></Route>
         </Routes>
-      </BrowserRouter>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
