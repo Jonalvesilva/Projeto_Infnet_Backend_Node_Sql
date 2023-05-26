1b384fadc696816a9f0f7bf661edc04d0bf95cd1
Jonathan Silva <jonalves.silva@jsmail.com.br>
2023-05-26 11:47:29 -0300

    edit dependentes controller

--- server/src/dependentes/dependentes.controller.ts
+++ server/src/dependentes/dependentes.controller.ts
@@ -1,16 +1,28 @@
 import express from "express";
+import * as dependentesService from "./dependentes.serv";
 
 export const dependentesController = express.Router();
 
 //Listagem de Dependentes
-dependentesController.get("/", async (req, res) => {
+dependentesController.get("/:idIntegrante", async (req, res) => {
   const limit = req.query.limit ? Number(req.query.limit) : undefined;
   const offset = req.query.offset ? Number(req.query.offset) : undefined;
   const order_by = req.query.order_by as string | undefined;
   const direction = req.query.direction as string | undefined;
   const search =
     req.query.search !== undefined ? req.query.search.toString() : undefined;
-  res.end("passei pelo controller dependentes");
+
+  const response = await dependentesService.getDepedentes(
+    {
+      limit,
+      offset,
+      search,
+      order_by,
+      direction,
+    },
+    +req.params.idIntegrante
+  );
+  res.status(200).json(response);
 });
 
 //Pega Dependente pelo ID
